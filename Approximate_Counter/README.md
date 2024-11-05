580e04909cf596075ed8238074c9edd713d8975a
PatrickBailey6 <pab6@email.sc.edu>
2024-11-04 23:56:32 -0500

    Finished Approximate Counter

---
+++ Approximate_Counter/README.md
@@ -0,0 +1,10 @@
+Useage:
+sloppySim <N_Threads> <Sloppiness> <work_time> <work_iterations> <CPU_BOUND> <Do_Logging>
+
+<N_Threads> : Number of created threads
+<Sloppiness> : Threshold before global count is updated
+<work_time> : Average amount of time each IO takes
+<work_iterations> : Amount of times the thread does work
+<CPU_BOUND> : true is CPU work, false is simulated IO work
+<Do_Logging> : Enable additional debug info
+
--- Approximate_Counter/sloppySim.cc
+++ Approximate_Counter/sloppySim.cc
@@ -12,55 +12,62 @@ using std::endl;
 
 
 struct shared {
+  int threshold;
   int n_threads;
   int work_time;
   int work_iterations;
   bool cpu_bound;
   bool do_logging;
+  int gcount;
+  pthread_mutex_t glock;
 };
 
 struct targs {
-  counter_t* c;
   shared* sh;
-  int local;
-  pthread_mutex_t llock;  // local lock
-};
-
-struct counter_t {
-  int global;
-  pthread_mutex_t glock;  // global lock
-  int threshold;
+  int thread_number;
 };
 
 /*  Args: 1. Global Counter 2. Thread Reference 3. Count to add*/
-void update(targs *ta, int amount) {
-  pthread_mutex_lock(&ta->llock);  // Lock Local
-  ta->local += amount;
-  if (ta->local >= ta->c->threshold) {
-    pthread_mutex_lock(&ta->c->glock);  // Lock Global
-    ta->c->global += ta->local; 
-    pthread_mutex_unlock(&ta->c->glock);  // Unlock Global
-    ta->local = 0;
-  }
-  pthread_mutex_unlock(&ta->llock);  // Unlock Local
+void update(targs *ta, int count) {
+  pthread_mutex_lock(&ta->sh->glock);  // Lock Global
+  ta->sh->gcount += count;
+  pthread_mutex_unlock(&ta->sh->glock);  // Unlock Global
 }
 
 void* my_thread(void* args) {
   targs* ta = (targs*) args;
-  if (ta->sh->cpu_bound) {  // CPU work being done
-    long increments = 1000 * ta->sh->work_time * 1000000;
-    for (long j = 0; j < increments; j++);
-    update(ta, 1);
-  } else {  // I/O work
-    usleep(1000000);
+  int count = 0;
+  for (int i = 0; i < ta->sh->work_iterations; ++i) {
+    if (ta->sh->cpu_bound) {  // CPU work being done
+      for (long j = 0; j < 1000000; j++);
+    }
+    else {  // I/O work
+      usleep((rand() % (3 * ta->sh->work_time) + ta->sh->work_time / 2) * 1000);
+    }
+    count++;
+    if(count >= ta->sh->threshold) {
+      update(ta, count);
+      count = 0;
+    }
   }
+  return NULL;
 }
 
 int get(targs *ta) {
-  pthread_mutex_lock(&ta->c->glock);
-  int val = ta->c->global;
-  pthread_mutex_unlock(&ta->c->glock);
+  pthread_mutex_lock(&ta->sh->glock);
+  int val = 0;
+  val = ta->sh->gcount;
+  pthread_mutex_unlock(&ta->sh->glock);
   return val;
+}
+
+void* log_thread(void* args) {
+  targs* ta = (targs*) args;
+  for(int i=0; i<10; i++) {
+    usleep(100000);
+    cout << "Global count = " << get(ta) << endl;
+  }
+  return NULL;
 }
 
 int main(int argc, char* argv[]) {
@@ -80,29 +87,42 @@ int main(int argc, char* argv[]) {
   if (argc > 5) cpu_bound = atoi(argv[5]);
   if (argc > 6) do_logging = atoi(argv[6]);
 
+  cout << "Running with:" << endl;
+  cout << n_threads << endl;
+  cout << sloppiness << endl;
+  cout << work_time << endl;
+  cout << work_iterations << endl;
+  cout << cpu_bound << endl;
+  cout << do_logging << endl;
+
   /* Load shared */
   shared sh; 
+  sh.gcount = 0;
+  sh.threshold = sloppiness;
   sh.n_threads = n_threads;
   sh.work_time = work_time;
   sh.work_iterations = work_iterations;
   sh.cpu_bound = cpu_bound;
   sh.do_logging = do_logging;
-  
-  /* Instantiate Global Counter */
-  counter_t gcount;
-  gcount.threshold = sloppiness;
 
   /* Start Threadding */
-  pthread_t threads[n_threads];
-  targs all_targs[n_threads];
+  pthread_t threads[n_threads+1];
+  targs all_targs[n_threads+1];
 
   for(int i =0;i<n_threads;++i) {
     all_targs[i].sh = &sh;
-    all_targs[i].local = 0;
+    all_targs[i].thread_number = i;
     pthread_create(&threads[i], NULL, my_thread, &all_targs[i]);
   }
+
+  pthread_create(&threads[n_threads], NULL, log_thread, &all_targs[n_threads]);
 
   for(int i=0; i<n_threads; ++i) {
     pthread_join(threads[i], NULL);
   }
+  if(do_logging)
+    pthread_join(threads[n_threads], NULL);
+
+  cout << "Final global count: " << sh.gcount << endl;
+  return 0;
 }
